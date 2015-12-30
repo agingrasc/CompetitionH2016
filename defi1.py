1ff7787b1f00efbc9f3215b0b0b9f5ccde2baa33
Félix Pelletier <felixpelletier23@gmail.com>
2015-12-29 19:37:14 -0500

    Bouger fonctionne

--- defi1.py
+++ defi1.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 import game_launcher
+from RULEngine.Util.Pose import Pose
+from RULEngine.Util.Position import Position
+from RULEngine.Game.Player import Player
 
-def boucle_principale(coach, terrain, arbitre, equipe_bleu, equipe_jaune):
-    print("Lol")
+class Defi1(object):
 
-game_launcher.start_game(boucle_principale)
+    a = 0
+    def boucle_principale(self, coach, terrain, etats, equipe_bleu, equipe_jaune):
+        if self.a == 0:
+            self.a = 1
+            coach.bouger(0, terrain.ball)
+
+
+
+defi = Defi1()
+game_launcher.start_game(defi.boucle_principale)
 
 
--- game_launcher.py
+++ game_launcher.py
@@ -1,16 +1,18 @@
 from RULEngine.Strategy.Strategy import Strategy
-from RULEngine.Util.Pose import Strategy
-from RULEngine.Util.Position import Strategy
+from RULEngine.Util.Pose import Pose
+from RULEngine.Util.Position import Position
+from RULEngine.Util import geometry
+from RULEngine.Game.Player import Player
+from RULEngine.Game.Ball import Ball
+from RULEngine.Command import Command
 from RULEngine.Framework import Framework
+
 import sys, time
 
 EVENT_SUCCEED = "success"
 EVENT_TIMEOUT = "timeout"
 EVENT_FAIL = "rate"
 EVENT_WIP = "inprogress"
-
-STATE_IDLE = "idle"
-STATE_MOVING = "moving"
 
 def getStrategy(main_loop):
     class DefiStrategy(Strategy):
@@ -19,16 +21,18 @@ def getStrategy(main_loop):
 
                 self.team.is_team_yellow = is_team_yellow
 
-                self.robot_states = [STATE_IDLE for robot in team.robots]
-                self.robot_events = [EVENT_SUCCEED for robot in team.robots]
-                self.robot_goals = [Position() for robot in team.robots] #Position vide. Position visée
-                self.robot_aim = [Position() for robot in team.robots] #Position vide. Position visée
+                self.robot_states = [self._idle for robot in team.players]
+                self.robot_events = [EVENT_SUCCEED for robot in team.players]
+                self.robot_goals = [Position() for robot in team.players] #Position vide. Position visée
+                self.robot_aim = [Position() for robot in team.players] #Position vide. Position visée
 
             def on_start(self):
-                main_loop(self, self.field, self.robot_event, self.team, self.opponent_team)
+                main_loop(self, self.field, self.robot_events, self.team, self.opponent_team)
+                self.execute()
 
             def execute(self):
-                for robot in robot_states:
+                for index, state in enumerate(self.robot_states):
+                    state(index)
 
             def on_halt(self):
                 self.on_start()
@@ -36,32 +40,91 @@ def getStrategy(main_loop):
             def on_stop(self):
                 self.on_start()
 
-            #----------Private---------
+            def _convertirPosition(self, position):
+                if isinstance(position, Player):
+                    return position.pose.position
+                elif isinstance(position, Ball):
+                    return position.position
+                elif isinstance(position, Position):
+                    return position
 
-            def succeed(joueur):
+
+            # ----------Private---------
+
+            def _succeed(self, joueur):
                 self.robot_events[joueur] = EVENT_SUCCEED
-                self.robot_states[joueur] = STATE_IDLE
+                self.robot_states[joueur] = self._idle
 
-            def fail(joueur):
+            def _fail(self, joueur):
                 self.robot_events[joueur] = EVENT_FAIL
-                self.robot_states[joueur] = STATE_IDLE
+                self.robot_states[joueur] = self._idle
 
-            def timeout(joueur):
+            def _timeout(self, joueur):
                 self.robot_events[joueur] = EVENT_TIMEOUT
-                self.robot_states[joueur] = STATE_IDLE
+                self.robot_states[joueur] = self._idle
 
-            def avancer(joueur):
-                """MoveTo position desiree"""
+            def _bouger(self, joueur):
+                position = self._convertirPosition(self.robot_goals[joueur])
 
-            def idle(joueur):
-                """MoveTo current position"""
+                player = self.team.players[joueur]
+                dist = geometry.get_distance(player.pose.position, position)
+                print(dist)
+                if 0 < dist < 150: # si la distance est exactement 0, la position n'est pas bonne
+                    self._succeed(joueur)
+                else:
+                    orientation = player.pose.orientation
+                    command = Command.MoveToAndRotate(player, self.team,
+                                                      Pose(position, orientation))
+                    self._send_command(command)
 
+            def _bougerPlusAim(self, joueur):
+                #TODO
+                position = self._convertirPosition(self.robot_goals[joueur])
 
-            #----------Public----------
-            def avancer(joueur, position):
-                pass
+                player = self.team.players[joueur]
+                dist = geometry.get_distance(player.pose.position, position)
+                if 0 < dist < 100: # si la distance est exactement 0, la position n'est pas bonne
+                    self._succeed(joueur)
+                else:
+                    orientation = player.pose.orientation
+                    command = Command.MoveToAndRotate(player, self.team,
+                                                      Pose(position, orientation))
+                    self._send_command(command)
+
+            def _idle(self, joueur):
+                player = self.team.players[joueur]
+                pose = player.pose
+                command = Command.MoveToAndRotate(player, self.team, pose)
+                self._send_command(command)
+
+            # ----------Public----------
+            def bouger(self, joueur, position, cible=None):
+                assert(isinstance(joueur, int))
+                assert(isinstance(position, (Position, Player, Ball, int)))
+                self.robot_goals[joueur] = position
+                if cible:
+                    self.robot_goals[joueur] = cible
+                    self.robot_states[joueur] = self._bougerPlusAim
+                else:
+                    self.robot_states[joueur] = self._bouger
+
+            def passe(self, joueur1, joueur2):
+                self._fail(joueur1)
+                self._fail(joueur2)
+
+            def lancer(self, joueur, cible, force=1):
+                self._fail(joueur)
+
+            def chercher_balle(self, joueur):
+                self._fail(joueur)
+
+            def positioner_entre_deux_ennemis(self, joueur, enemi1, enemi2):
+                self._fail(joueur)
+
+    return DefiStrategy
+
 
 def start_game(main_loop):
 
     framework = Framework()
-    framework.start_game(DefiStrategy)
+    framework.start_game(getStrategy(main_loop))
